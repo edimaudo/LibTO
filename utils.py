7ca5ca4e0176573350c4e66731e1c8bda311ad62
Edima <edimaudo@gmail.com>
2026-02-14 15:16:22 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,44 @@
 
+# Libraries
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os, os.path
+import warnings
+import random
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import json
+import datetime
+from datetime import datetime
+import time
+import matplotlib
+import statistics
+import scipy
+from scipy.stats import linregress
+import sklearn
+from sklearn.linear_model import LinearRegression
+from pmdarima import auto_arima
+from google import genai
+from elasticsearch import Elasticsearch
+from dotenv import load_dotenv, dotenv_values 
+
+
+"""
+App Information
+"""
+APP_NAME = 'LibTO'
+ABOUT_HEADER = 'About'
+OVERVIEW_HEADER = 'Overview'
+BRANCH_INTELLIGENCE_HEADER = "Branch Intelligence"
+BRANCH_PROGRAM_EVENT_HEADER = 'Branch Program & Events'
+APP_FILTERS = 'Filters'
+NO_DATA_INFO = 'No data available to display based on the filters'
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+st.set_page_config(
+    page_title=APP_NAME,
+    layout="wide"
+)
