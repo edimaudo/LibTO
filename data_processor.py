13e2663e73048734bafe788ab704b1ee98570079
Edima <edimaudo@gmail.com>
2026-02-14 10:11:31 -0500

    Change avg_ws to return integer instead of rounded

--- data_processor.py
+++ data_processor.py
@@ -52,7 +52,7 @@ class DataProcessor:
             return {
                 "count": num_libraries,
                 "avg_sq_ft": f"{avg_sq_ft:,.0f}",
-                "avg_ws": round(avg_ws, 0),
+                "avg_ws": int(avg_ws),
                 "oldest": oldest_info
             }
 
