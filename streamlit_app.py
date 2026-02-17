0f2700922a838ff2e0ca14e5a5cf011727a39652
Edima <edimaudo@gmail.com>
2026-02-17 21:43:03 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from utils import *
 
 pg = st.navigation([
     st.Page("Overview.py"),
-    st.Page("Branch_intelligence.py"),
+    st.Page("Branch_Intelligence.py"),
     #st.Page("Branch_program_event.py"),
 ])
 pg.run()
