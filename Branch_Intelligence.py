c85d2ed9508aea1755f01ae6a75147b368005557
Edima <edimaudo@gmail.com>
2026-05-12 17:16:03 +0000

    update

--- Branch_Intelligence.py
+++ Branch_Intelligence.py
@@ -4,9 +4,50 @@ from data import *
 st.title(APP_NAME)
 st.header(BRANCH_INTELLIGENCE_HEADER)
 
-branch_option = st.sidebar.selectbox("Branches",branch_list)
+# setup data
+branch_option = st.sidebar.selectbox("Branch",branch_list)
 branch_df = physical[(physical['BranchName'] == branch_option)].reset_index()
+print(branch_df)
+df_events_with_branch_info = pd.merge(
+    df_events,
+    branch_df,
+    left_on='LocationName',
+    right_on='BranchName',
+    how='inner' 
+)
+columns_to_filter = [
+    'Title',
+    'StartTime',
+    'EndTime',
+    'StartDateLocal',
+    'LocationName',
+    'Audiences',
+    'Languages',
+    'EventTypes'
+]
 
+df_filtered_events = df_events_with_branch_info[columns_to_filter].drop_duplicates()
+
+df_filtered_events.rename(columns={
+    'Title': 'Event title',
+    'StartTime': 'Start Time',
+    'EndTime': 'End Time',
+    'StartDateLocal': 'Event Date',
+    'LocationName': 'Location',
+    'Audiences': 'Audience',
+    'Languages': 'Language',
+    'EventTypes': 'Event Types'
+}, inplace=True)
+
+# clean up date and time
+# Convert 'Start Time' and 'End Time' to datetime objects and then format them
+df_filtered_events['Start Time'] = pd.to_datetime(df_filtered_events['Start Time']).dt.strftime('%I:%M%p')
+df_filtered_events['End Time'] = pd.to_datetime(df_filtered_events['End Time']).dt.strftime('%I:%M%p')
+
+# Convert 'Event Date' to datetime objects and then format it to show only the date
+df_filtered_events['Event Date'] = pd.to_datetime(df_filtered_events['Event Date']).dt.strftime('%Y-%m-%d')
+
+# setup containers
 top_container = st.container()
 middle_container = st.container()
 bottom_container = st.container()
@@ -42,8 +83,7 @@ with middle_container:
         st.write("Leading Reading Avilable" + " : " + str("Yes" if branch_df['LeadingReading'][0] == 1 else "No"))
 
 with bottom_container:
-    option = st.selectbox("Options",("Trends", "Forecasts","Events" ,"Branch Agent"))
-    st.subheader("Branch Trends")
+    option = st.selectbox("Options",("Trends", "Forecasts","Events" ,"Branch Q&A"))
     branch_code = branch_df['BranchCode'][0]
     # Clean columns of trend dataframes for matching
     df_card_registration.columns = df_card_registration.columns.str.strip()
@@ -55,6 +95,7 @@ with bottom_container:
     branch_visits = df_visits[df_visits['BranchCode'] == branch_code]
     branch_workstation_usage = df_workstation_usage[df_workstation_usage['BranchCode'] == branch_code]
     if option == "Trends":
+        st.subheader("Branch Trends")
         if len(branch_code) > 0:
             col1, col2 = st.columns(2)
 
@@ -153,9 +194,27 @@ with bottom_container:
         with col2:
             st.plotly_chart(visits_forecast)
             st.plotly_chart(workstation_forecast)
-    elif option == "Event":
-        pass
-    elif option == "Branch Agent":
-        pass
+    elif option == "Events":
+        st.dataframe(df_filtered_events)
+    elif option == "Branch Q&A":
+        st.write("Ask a question about " + str(branch_df['BranchName'][0]))
+        branch_question_txt = st.text_area(label="",value="",placeholder=None,key=1)
+        branch_question_button = st.button("Get Inisghts", type="primary")
+        instruction="""
+                Return your entire response in professional Markdown format:
+                1. Use ## for Section Headers.
+                2. Use **bold** for key insights and numbers.
+                3. Use Markdown tables if comparing multiple data points.
+                4. If there is a clear trend, provide a 'Key Takeaway' section at the end.
+            """
+        if branch_question_button:
+            st.html("<p> </p>")
+            if branch_question_txt.strip():    
+                st.subheader("Insights")
+                with st.spinner("Analyzing data..."):
+                    analysis = analyze_dataframe(df_master, branch_question_txt,instruction)
+                    st.markdown(analysis)
+            else:
+                st.warning("Please enter a question in the text area above.")
 
     
