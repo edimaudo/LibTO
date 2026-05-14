4127a1fa36a32a51dd6ad50ad49ebb93ac678863
Edima <edimaudo@gmail.com>
2026-05-14 15:44:39 +0000

    update

--- data.py
+++ data.py
@@ -398,9 +398,10 @@ def route_query(user_input):
     return is_event_query, is_trend_query
 
 # Gemini Model
-api_key = os.getenv("GEMINI_API_KEY")
-genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-2.5-flash")
+# api_key = os.getenv("GEMINI_API_KEY")
+# genai.configure(api_key=api_key)
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("models/gemini-2.5-flash") #model = genai.GenerativeModel("gemini-2.5-flash")
 instruction="""
                 Return your entire response in professional Markdown format:
                 1. Use ## for Section Headers. The sections are 'Question','Analysis','Insight'
@@ -427,5 +428,8 @@ def analyze_dataframe(df, user_query, instructions="Provide a clear, concise ana
     """
     
     # 3. Get the response
-    response = model.generate_content(prompt)
+    response = client.models.generate_content(model=model, contents=prompt) # response = model.generate_content(prompt)
     return response.text
+
+
+
