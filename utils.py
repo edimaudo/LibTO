3f217efa3178d15f455e9d14ca752857bfbbed28
Edima <edimaudo@gmail.com>
2026-05-14 15:30:33 +0000

    update

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ from sklearn.linear_model import LinearRegression
 import pmdarima as pm
 from pmdarima import auto_arima
 from google import genai
-import google.generativeai as genai
+#import google.generativeai as genai
 from dotenv import load_dotenv, dotenv_values 
 from functools import reduce
 
